672ade465a8c64cfd0df67b78ceda2bf19a96d04
Karim B <mattiakarim@live.it>
2019-06-18 21:25:45 +0200

    more changes

--- todo/src/store/actions/actions.js
+++ todo/src/store/actions/actions.js
@@ -1,3 +1,4 @@
 export const TODO = 'TODO';
 export const COMPLETE = 'COMPLETE';
 export const DELETE_TODO = 'DELETE_TODO';
+export const ADD_LIST = 'ADD_LIST';
--- todo/src/store/reducers/reducer.js
+++ todo/src/store/reducers/reducer.js
@@ -1,13 +1,20 @@
-import { TODO, COMPLETE, DELETE_TODO } from '../actions/actions';
+import { TODO, COMPLETE, DELETE_TODO, ADD_LIST } from '../actions/actions';
 
 const defaultState = {
     todos: [],
+    todoList: [],
 }
 
 const reducer = (state = defaultState, action) => {
     switch (action.type) {
         case TODO: 
-            return {...state, todos: [action.payload].concat(state.todos)};
+            let newtodoList = state.todoList.map(list => {
+                if(list.id === action.listID){
+                    return {...list, todos: [action.payload].concat(state.todos)};
+                }
+                return list
+            }) 
+            return {...state, todos: [action.payload].concat(state.todos), todoList: newtodoList}
         case COMPLETE: 
             const newTodosArr = state.todos.map(todo => {
                 if(todo.id === action.id) {
@@ -19,6 +26,8 @@ const reducer = (state = defaultState, action) => {
         case DELETE_TODO:
             const newTodosArr2 = state.todos.filter(todo => todo.id !== action.id);
             return {...state, todos: newTodosArr2 }
+        case ADD_LIST:
+            return {...state, todoList: state.todoList.concat({todos: {...state.todos}, id: action.id})}
         default:
             return state;
     }
